d32390737785de7f6a418923636bb7f9f69b23ef
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-03-12 18:05:08 +0530

    comments

--- src/Personal_Assistant/Assistant.py
+++ src/Personal_Assistant/Assistant.py
@@ -1,20 +1,28 @@
 import datetime
 
+# for taking the time for wishMe method
 import pyttsx3
 
+# for taking the voice from the window and the laptop
+
 engine = pyttsx3.init('Sapi5')
+# sapi 5 voice is the default voice for windows
 voices = engine.getProperty('voices')
+# getter method for the pyttsx3
 engine.setProperty('voice', voices[0].id)
 
 
+# setter method for the pyttsx3
+
+
 def speak(audio):
-    engine.say(audio)
+    engine.say(audio)  # Method for the speaking of the the assistant
     engine.runAndWait()
 
 
 def WishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
+    if hour >= 0 and hour < 12:  # Method for Wishing the master
         speak('Good morning sir')
     elif hour >= 12 and hour < 18:
         speak("Good afternoon sir")
