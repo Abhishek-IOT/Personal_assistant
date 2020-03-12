b2bfc94ad4bbd026fd3c123d522110105af4b5b9
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-03-12 17:55:11 +0530

    creating the speak fn of the voice Assistant.py

--- src/Personal_Assistant/Assistant.py
+++ src/Personal_Assistant/Assistant.py
@@ -3,3 +3,6 @@ import pyttsx3
 engine=pyttsx3.init('Sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice',voices[0].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
