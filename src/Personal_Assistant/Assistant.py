85e8a135ab0a8e1772ad006ec54141fc6656f12f
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-03-31 18:41:33 +0530

    update the tell date method for telling the methods

--- src/Personal_Assistant/Assistant.py
+++ src/Personal_Assistant/Assistant.py
@@ -75,7 +75,7 @@ def telldate():
     todaydate = day[8:10]
     this_Month = day[5:7]
     print(todaydate + "")
-    # print(this_Month)
+    print(this_Month)
     this_year = day[0:4]
     # print(this_year)
 
@@ -85,7 +85,36 @@ def telldate():
         dateno = "Twenty First"
     if todaydate == '31':
         dateno = "Thirty First"
-    speak(dateno)
+    elif todaydate == '2':
+        dateno = "Second"
+    elif todaydate == '3':
+        dateno = "Third"
+    elif this_Month == '01':
+        month = "January"
+    elif this_Month == '02':
+        month = "February"
+    if this_Month == '03':
+        month = "March"
+    if this_Month == '04':
+        month = "April"
+    if this_Month == '05':
+        month = 'May'
+    if this_Month == '06':
+        month = 'June'
+    if this_Month == '07':
+        month = 'July'
+    if this_Month == '08':
+        month = 'August'
+    if this_Month == '09':
+        month = 'September'
+    if this_Month == '10':
+        month = 'October'
+    if this_Month == '11':
+        month = 'November'
+    if this_Month == '12':
+        month = 'December'
+
+    speak(dateno + month + this_year)
 
 
 def telltime():
