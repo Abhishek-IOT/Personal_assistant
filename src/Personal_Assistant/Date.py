5a790c721a626d4268d55c7bdd8d389ee3b17ced
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-04-02 16:49:03 +0530

    inherting the properties Speaking class so that speak and many more method can be used in Date.py class

--- src/Personal_Assistant/Date.py
+++ src/Personal_Assistant/Date.py
@@ -1,7 +1,9 @@
 import datetime
 
+from src.Personal_Assistant.Speaking import Speaking
 
-class Date:
+
+class Date(Speaking):
     def __int__(self):
         pass
 
@@ -11,3 +13,5 @@ class Date:
         hour = time[11:13]
         min = time[14:16]
         sec = time[18:19]
+        print(hour + min + sec)
+        Speaking.speak(hour + "Hours" + min + "Minutes" + sec + "Seconds sir")
