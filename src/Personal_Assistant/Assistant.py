4782134c6401f1fcecb12cd893034484816820ab
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-04-02 16:44:34 +0530

    reformated the code for the OOPS Approach

--- src/Personal_Assistant/Assistant.py
+++ src/Personal_Assistant/Assistant.py
@@ -12,57 +12,14 @@ Personal Assistant project
 # for taking the voice from the window and the laptop
 # importing the wikipedia module for having a search from wikipedia
 
-# engine=pyttsx3.init('dummy')
-# if sapi5 is giving any error you can first try with the dummy parameter and then with the sapi5 parameter and also remember the case used in it
-# engine = pyttsx3.init('sapi5')
-# sapi 5 voice is the default voice for windows
-# voices = engine.getProperty('voices')
-# getter method for the pyttsx3
-# engine.setProperty('voice', voices[0].id)
+import webbrowser
 
+import wikipedia
 
-# setter method for the pyttsx3
-
-
-# def speak(audio):
-#   engine.say(audio)  # Method for the speaking of the the assistant
-#  engine.runAndWait()
-
-
-# def WishMe():
-#   hour = int(datetime.datetime.now().hour)
-#   if hour >= 0 and hour < 12:  # Method for Wishing the master
-#      speak('Good morning sir')
-# elif hour >= 12 and hour < 18:
-#    speak("Good afternoon sir")
-# else:
-#   speak('Good evening sir')
-# speak('I am Jarvis   .Please tell how may I help u Sir')
 from src.Personal_Assistant.Speaking import Speaking
 
 """
-def takeCommand():
-# this method is for taking the commands and recognizing the command
-    r = sr.Recognizer()
-# from the speech_Recognition module we will use the recongizer method for recognizing
-    with sr.Microphone() as source:
-        #from the speech_Recognition module we will use the Microphone module for listening the command
-        print('Listening')
-        r.pause_threshold = 1
-        # seconds of non-speaking audio before a phrase is considered complete
-        audio = r.listen(source)
-        try:
-            print("Recognizing")
-            Query = r.recognize_google(audio, language='en-in')
-            # for listening the command in indian english
-            print("the query is printed='", Query, "'")
-            # for printing the query or the command that we give
-        except Exception as e:
-            print(
-                e)  # this method is for handling the exception and so that assistant can ask for telling again the command
-            print("Say that again sir")
-            return "None"
-        return Query
+
 
 
 
@@ -138,37 +95,36 @@ def telltime():
 
 """
 if __name__ == '__main__':  # main method for executing the functions
-     speaking = Speaking()
-
-"""
-    WishMe()
-    query = takeCommand().lower()
+    speaking = Speaking()
+    speaking.WishMe()
+    query = speaking.takeCommand().lower()
     if 'wikipedia' in query:
-        speak("Checking in the wikipedia Sir")
+        speaking.speak("Checking in the wikipedia Sir")
         query = query.replace("wikipedia", "")
         result = wikipedia.summary(query, sentences=4)
-        speak("According to wikipedia Sir")
-        speak(result)
+        speaking.speak("According to wikipedia Sir")
+        speaking.speak(result)
     elif 'open youtube' in query:
-        speak("Opening the youtube sir")
+        speaking.speak("Opening the youtube sir")
         webbrowser.open("www.youtube.com")
     elif 'open google' in query:
-        speak("Opening the google sir")
+        speaking.speak("Opening the google sir")
         webbrowser.open("google.com")
     elif 'open facebook' in query:
-        speak("Opening facebook sir")
+        speaking.speak("Opening facebook sir")
         webbrowser.open("facebook.com")
     elif 'open geeksforgeeks' in query:
-        speak("Opening geeks for geeks sir")
+        speaking.speak("Opening geeks for geeks sir")
         webbrowser.open("geeksforgeeks.com")
     elif 'open the news' in query:
-        speak("Telling about the news sir")
+        speaking.speak("Telling about the news sir")
         webbrowser.open("https://www.indiatvnews.com/")
     # speak(webbrowser.open("https://www.indiatvnews.com/"))
     elif 'open online classes' in query:
-        speak("Opening the cousresite.com sir")
+        speaking.speak("Opening the cousresite.com sir")
         webbrowser.open("https://blackboard.coursesites.com/")
-        speak("Goodluck sir for studies")
+        speaking.speak("Goodluck sir for studies")
+        """
     elif 'time' in query:
         telltime()
     elif 'date' in query:
