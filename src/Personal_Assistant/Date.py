1395cb30c89feba738fada14e827cfa5cac8dde2
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-04-02 16:47:25 +0530

    importing the datetime module for taking the date and time

--- src/Personal_Assistant/Date.py
+++ src/Personal_Assistant/Date.py
@@ -1,3 +1,13 @@
+import datetime
+
+
 class Date:
     def __int__(self):
         pass
+
+    def telltime(self):
+        time = str(datetime.datetime.now())
+        print(time)
+        hour = time[11:13]
+        min = time[14:16]
+        sec = time[18:19]
