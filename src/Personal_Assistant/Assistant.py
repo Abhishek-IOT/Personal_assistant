a7b65ad10a7b105b237cb40ddfcda5f4a1b9643b
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-04-03 16:59:44 +0530

    used a keyword class and had used a infinite loop so that my personal Assistant.py can take commands infinite times

--- src/Personal_Assistant/Assistant.py
+++ src/Personal_Assistant/Assistant.py
@@ -20,86 +20,80 @@ from src.Personal_Assistant.Date import Date
 from src.Personal_Assistant.Speaking import Speaking
 from src.Personal_Assistant.Time import Time
 
-"""
+
+class Assistant:
+    if __name__ == '__main__':  # main method for executing the functions
+        speaking = Speaking()
+        speaking.WishMe()
+        time = Time()
+        while (True):
+            query = speaking.takeCommand().lower()
+            if 'wikipedia' in query:
+                speaking.speak("Checking in the wikipedia Sir")
+                query = query.replace("wikipedia", "")
+                result = wikipedia.summary(query, sentences=4)
+                speaking.speak("According to wikipedia Sir")
+                speaking.speak(result)
+                continue
+            elif 'open youtube' in query:
+                speaking.speak("Opening the youtube sir")
+                webbrowser.open("www.youtube.com")
+                continue
+            elif 'open google' in query:
+                speaking.speak("Opening the google sir")
+                webbrowser.open("google.com")
+                continue
+            elif 'open facebook' in query:
+                speaking.speak("Opening facebook sir")
+                webbrowser.open("facebook.com")
+                continue
+            elif 'open geeksforgeeks' in query:
+                speaking.speak("Opening geeks for geeks sir")
+                webbrowser.open("geeksforgeeks.com")
+                continue
+            elif 'open the news' in query:
+                speaking.speak("Telling about the news sir")
+                webbrowser.open("https://www.indiatvnews.com/")
+                continue  # speak(webbrowser.open("https://www.indiatvnews.com/"))
+            elif 'open online classes' in query:
+                speaking.speak("Opening the cousresite.com sir")
+                webbrowser.open("https://blackboard.coursesites.com/")
+                speaking.speak("Goodluck sir for studies")
+                continue
+            elif 'time' in query:
+                time.tellTime()
+                continue
+            elif "jarvis bye" in query:
+                speaking.speak("Bye sir  Have a very good day sir.Take care")
+                exit()
 
 
+            elif 'date' in query:
+                date = Date()
+                date.telldate()
+                continue
+            elif 'set reminder' in query:
+                speaking.speak("at which hour you want to set the reminder sir")
+                # speak("tell us the hour sir")
 
+                query = speaking.takeCommand().lower()
+                if query[0] == '1':
+                    if query[1] == '2' or query[1] == '0' or query[1] == '1':
+                        userhour = query[0:2]
+                        usermin = query[3:5]
+                        time_meridiem = query[6:9]
+                        print(time_meridiem)
+                else:
+                    userhour = query[0]
+                    usermin = query[2:4]
+                    time_meridiem = query[5:8]
+                    print(time_meridiem)
 
-def set_Reminder(userhour):
-    time = str(datetime.datetime.now())
-    print(time)
-    hour = time[11:13]
-    min = time[14:16]
-    if userhour == hour:
-        speak("Sir your reminder")
-
-
-
-"""
-if __name__ == '__main__':  # main method for executing the functions
-    speaking = Speaking()
-    speaking.WishMe()
-    time = Time()
-    query = speaking.takeCommand().lower()
-    if 'wikipedia' in query:
-        speaking.speak("Checking in the wikipedia Sir")
-        query = query.replace("wikipedia", "")
-        result = wikipedia.summary(query, sentences=4)
-        speaking.speak("According to wikipedia Sir")
-        speaking.speak(result)
-    elif 'open youtube' in query:
-        speaking.speak("Opening the youtube sir")
-        webbrowser.open("www.youtube.com")
-    elif 'open google' in query:
-        speaking.speak("Opening the google sir")
-        webbrowser.open("google.com")
-    elif 'open facebook' in query:
-        speaking.speak("Opening facebook sir")
-        webbrowser.open("facebook.com")
-    elif 'open geeksforgeeks' in query:
-        speaking.speak("Opening geeks for geeks sir")
-        webbrowser.open("geeksforgeeks.com")
-    elif 'open the news' in query:
-        speaking.speak("Telling about the news sir")
-        webbrowser.open("https://www.indiatvnews.com/")
-    # speak(webbrowser.open("https://www.indiatvnews.com/"))
-    elif 'open online classes' in query:
-        speaking.speak("Opening the cousresite.com sir")
-        webbrowser.open("https://blackboard.coursesites.com/")
-        speaking.speak("Goodluck sir for studies")
-
-    elif 'time' in query:
-        time.tellTime()
-    elif "jarvis bye" in query:
-        speaking.speak("Bye sir  Have a very good day sir.Take care")
-        exit()
-
-
-    elif 'date' in query:
-        date = Date()
-        date.telldate()
-
-    elif 'set reminder' in query:
-        speaking.speak("at which hour you want to set the reminder sir")
-        # speak("tell us the hour sir")
-        c = 0
-        query = speaking.takeCommand().lower()
-        if query[0] == '1':
-            if query[1] == '2' or query[1] == '0' or query[1] == '1':
-                userhour = query[0:2]
-                usermin = query[3:5]
-                time_meridiem = query[6:9]
-                print(time_meridiem)
-        else:
-            userhour = query[0]
-            usermin = query[2:4]
-            time_meridiem = query[5:8]
-            print(time_meridiem)
-
-        speaking.speak("Setting the reminder at" + userhour + "hour" + usermin + "Minuetes sir")
-        list = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-        if userhour in list:
-            userhour = "0" + userhour
-        time.set_Reminder(userhour, usermin, time_meridiem)
-        print("the reminder hour" + userhour)
-        print("the reminder min" + usermin)
+                speaking.speak("Setting the reminder at" + userhour + "hour" + usermin + "Minuetes sir")
+                list = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+                if userhour in list:
+                    userhour = "0" + userhour
+                time.set_Reminder(userhour, usermin, time_meridiem)
+                print("the reminder hour" + userhour)
+                print("the reminder min" + usermin)
+                continue
