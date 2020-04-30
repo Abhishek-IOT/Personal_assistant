864b95f7494634cdff1ef8795db5eba2fdfbf152
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-04-30 11:22:57 +0530

    made a list for hindi no and imported the Speaking class

--- src/Personal_Assistant/Number_in_hindi.py
+++ src/Personal_Assistant/Number_in_hindi.py
@@ -1,2 +1,8 @@
-class Number_in_hindi:
+from src.Personal_Assistant.Speaking import Speaking
+
+
+class Number_in_hindi(Speaking):
     def TakeNumber(self):
+        list = {"Setis": 37, "Adhtis": 38, "Untaalis": 39, "Iktalis": 41, "Byalis": 42, "Tetalis": 43, "Chavalis": 44,
+                "Pentalis": 45, "Chyalis": 46, "Setalis": 47, "Adtalis": 48, "Unachas": 49, "Baavan": 52, "Tirepan": 53,
+                "	Chauvan": 54, "Chappan": 56, "Satavan": 57, "Athaavan": 58, "Unsadh": 59, "Iksadh": 61}
