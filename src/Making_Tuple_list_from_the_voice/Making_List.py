e7f72477b22004dac239241e4c48b8381cc8b6ff
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-16 17:18:57 +0530

    Made the function a More interative so that it can help me in the elements adding

--- src/Making_Tuple_list_from_the_voice/Making_List.py
+++ src/Making_Tuple_list_from_the_voice/Making_List.py
@@ -8,7 +8,9 @@ class Making_list(Speaking):
         query = speaking.takeCommand()
         query = (int)(query)
         list = []
+        speaking.speak("Tell me the elements in the list sir")
         for i in range(query):
             item = speaking.takeCommand()
             list.append(item)
+            speaking.speak("the" + item + "is added in the list sir")
         print(list)
