c6b1afec845ad4058ab799e518b514cb3bd602f9
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-16 15:47:53 +0530

    completed the list of numbers which I don not know in hindi

--- src/Calculations/Number_to_Hindi.py
+++ src/Calculations/Number_to_Hindi.py
@@ -5,4 +5,13 @@ class Number_to_Hindi:
     speaking = Speaking()
 
     def Numbers(self, no):
-        list1 = {37: 'Setis', 38: 'Adhtis', 39: 'Untaalis', 41: 'Iktalis', 42: 'Byalis', 43}
+        list1 = {37: 'Setis', 38: 'Adhtis', 39: 'Untaalis', 41: 'Iktalis', 42: 'Byalis', 43: 'Tetalis',
+                 44: 'Chavalis', 45: 'Pentalis', 46: 'Chyalis', 47: 'Setalis', 48: 'Adtalis', 49: 'Unachas',
+                 57: 'Satavan', 58: 'Athaavan', 59: 'Unsadh', 61: 'Iksadh', 62: 'Baasad', 63: 'Tirsadh', 64: 'Chausadh',
+                 65: 'Pensadh', 67: 'Sadhsadh', 68: 'Asdhsadh', 69: 'Unahtar', 71: 'Ikahtar', 73: 'Tihatar',
+                 74: 'Chauhatar',
+                 75: 'Pachhatar', 76: 'Chiyahatar', 77: 'Satahatar', 78: 'Adhahatar', 79: 'Unnasi', 81: 'Ikyasi',
+                 82: 'Byaasi',
+                 83: 'Tirasi', 84: 'Chaurasi', 85: 'Pachasi', 86: 'Chiyaasi', 87: 'Sataasi', 88: 'Athasi', 89: 'Nauasi',
+                 91: 'Ikyaanave', 92: 'Baanave', 93: 'Tiranave', 94: 'Chauraanave', 95: 'Pachaanave', 96: 'Chiyaanave',
+                 97: 'Sataanave', 98: 'Adhaanavek'}
