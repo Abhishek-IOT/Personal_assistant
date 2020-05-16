c0bae4e2f3e593d8c62a4a650773e51678292d3b
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-16 15:35:24 +0530

    making the list so that it can tell me the number in hindi

--- src/Calculations/Number_to_Hindi.py
+++ src/Calculations/Number_to_Hindi.py
@@ -3,3 +3,6 @@ from src.Personal_Assistant.Speaking import Speaking
 
 class Number_to_Hindi:
     speaking = Speaking()
+
+    def Numbers(self, no):
+        list1 = {37: 'Setis', 38: 'Adhtis', 39: 'Untaalis', 41: 'Iktalis', 42: 'Byalis', 43}
