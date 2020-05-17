932da1c940ce1f11ef070c3e692b88be928b0fab
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-17 18:38:01 +0530

    making a function so that it add infinite elements in list .It will be used in the case when user is not having the count of the element of the list.

--- src/Making_Tuple_list_from_the_voice/Making_List.py
+++ src/Making_Tuple_list_from_the_voice/Making_List.py
@@ -14,3 +14,16 @@ class Making_list(Speaking):
             list.append(item)
             speaking.speak("the" + item + "is added in the list sir")
         print(list)
+
+    def List_Making_Do_not_have_No(self):
+        speaking = Speaking()
+        i = 0
+        while (True):
+            query = speaking.takeCommand()
+            speaking.speak(i + "Element is " + query)
+            speaking.speak("do you want to stop adding sir")
+            query1 = speaking.takeCommand()
+            if (query1 == 'yes'):
+                break
+            else:
+                continue
