54e23a69358950dff719bf2c99074b8616baee57
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-05-19 17:57:08 +0530

    Made the Basic interface and will update and add more style to it.

--- src/Graphic_User_Interface/GUI.py
+++ src/Graphic_User_Interface/GUI.py
@@ -1,12 +1,12 @@
 from tkinter import *
-
+from tkinter import ttk
 
 class GUI:
     root = Tk()
-    root.geometry("500x100")
-
+    root.geometry("500x30")
+    root.title("Abhi")
+    Label1 = ttk.Label(root, text="Query")
+    Label1.grid(row=0, column=0)
+    Entry1 = ttk.Entry(root, width=75)
+    Entry1.grid(row=0, column=1)
     root.mainloop()
-
-
-if __name__ == '__main__':
-    G = GUI()
