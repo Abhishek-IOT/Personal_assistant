408fa1b17c685559e4f9f47de9ea4d49b21dbac7
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-06-21 08:39:53 +0530

    made a clock GUI for time showing

--- src/Graphic_User_Interface/Stopwatch.py
+++ src/Graphic_User_Interface/Stopwatch.py
@@ -2,18 +2,20 @@ import time
 import tkinter as tk
 
 
-class Stopwatch:
-    def displaytime(self):
-        current_time = time.strftime('%H:%M:%S')
+class Digital_clock:
+    def __init__(self, window):
+        self.window = window
+        self.window.title('Digital clock')
+        self.clock_Label = tk.Label(self.window, font='ariel 80', bg='white', fg='red')
+        self.clock_Label.grid(row=0, column=1)
+        self.display_the_time()
+
+    def display_the_time(self):
+        self.current_time = time.strftime('%H:%M:%S')
+        self.clock_Label['text'] = self.current_time
+        self.window.after(200, self.display_the_time)
 
 
-    def Digital_clock(self):
-        root = tk.Tk()
-        root.title("Take rest")
-        label = tk.Label(root, font='ariel 80', bg='white', fg="red")
-        root.mainloop()
-
-
-if __name__ == '__main__':
-    stop = Stopwatch()
-    stop.Digital_clock()
+mywindow = tk.Tk()
+clock = Digital_clock(mywindow)
+mywindow.mainloop()
