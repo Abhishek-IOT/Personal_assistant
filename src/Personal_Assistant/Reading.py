baafa5c9408858e66ef8639aab899bbcc15506b5
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-10-02 19:12:27 +0530

    This is a class for making the Assistant read for me .

---
+++ src/Personal_Assistant/Reading.py
@@ -0,0 +1,18 @@
+import PyPDF2
+import pyttsx3
+
+
+class Reading:
+    book = open('E:\\Bhagavad-gita_As_It_Is.pdf', 'rb')
+    pdfReader = PyPDF2.PdfFileReader(book)
+    paged = pdfReader.numPages
+    print(paged)
+    speak = pyttsx3.init()
+    from_page = pdfReader.getPage(12)
+    text = from_page.extractText()
+    speak.say(text)
+    speak.runAndWait()
+
+
+if __name__ == '__main__':
+    Reading()
