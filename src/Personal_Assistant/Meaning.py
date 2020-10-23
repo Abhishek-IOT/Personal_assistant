6461d1ea105d8fa4dec48e09aacac4054e028cc6
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-10-23 20:48:15 +0530

    Completed the class Meaning.py and it will led the assistant to tell me the meaning of the word.

--- src/Personal_Assistant/Meaning.py
+++ src/Personal_Assistant/Meaning.py
@@ -1,3 +1,15 @@
 from PyDictionary import PyDictionary
 
-dic = PyDictionary()
+from src.Personal_Assistant.Speaking import Speaking
+
+
+class Meaning:
+    dic = PyDictionary()
+    speak = Speaking()
+    speak.speak("Which word do u want to find the meaning sir")
+    query = speak.takeCommand()
+    word = dic.meaning(query)
+    print(len(word))
+    for states in word:
+        print(word[states])
+        speak.speak(word[states])
