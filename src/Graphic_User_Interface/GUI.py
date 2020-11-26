4058b286899c0d499123e5c5ecc7f946b3822b0f
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-11-26 19:14:28 +0530

    trying to make the gui of the assistant.

--- src/Graphic_User_Interface/GUI.py
+++ src/Graphic_User_Interface/GUI.py
@@ -1,18 +1,8 @@
 from tkinter import *
-from tkinter import ttk
-
-
 
 
 class GUI:
-    def Graphics(self):
-        root = Tk()
-        root.geometry("500x30")
-        root.title("Abhi")
-        Label1 = ttk.Label(root, text="Query",command=query)
-        Label1.grid(row=0, column=0)
-        Entry1 = ttk.Entry(root, width=75)
-        Entry1.grid(row=0, column=1)
-        root.mainloop()
-if __name__ == '__main__':
-    GUI().Graphics()
+    root = Tk()
+    root.geometry("500x500")
+    root.title("Year")
+    root.mainloop()
